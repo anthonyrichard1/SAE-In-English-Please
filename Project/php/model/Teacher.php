e26db7575b78ee16a59203dcf601c54ea79d9075
patrick.brugiere <patrick.brugiere@etu.uca.fr>
2023-10-25 11:59:43 +0200

    travail sur la classe teacher

--- Project/php/model/Teacher.php
+++ Project/php/model/Teacher.php
@@ -1,18 +1,24 @@
 <?php
 namespace modeles;
+
 class Teacher extends User
 {
+    protected array $vocab;
 
-    protected function createVocabulary(){
+    public function __construct(int $id, string $mail, string $name, string $surname, array $vocab) {
+        parent::__construct($id, $mail, $name, $surname);
+        $this->vocab = $vocab;
+    }
 
+    public function createVocabulary($array = [], String $name_test, String $url_image):{
+        $a = new Vocabulary($array,$name_test,$url_image);
+        $vocab[] = $a;
+        return $a;
     }
 
     protected function modifyVocabulary(){
 
     }
-
-
-
 
 
 }
