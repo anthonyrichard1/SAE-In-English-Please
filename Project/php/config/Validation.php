e70e46f99fb06769e10e390e8f41073491acafc6
anthony.richard <anthony.richard@etu.uca.fr>
2023-11-13 14:08:57 +0100

    fonctions de validation pour la plupart des entrées + pour l'action

--- Project/php/config/Validation.php
+++ Project/php/config/Validation.php
@@ -1,17 +1,35 @@
 <?php
 namespace config;
 
+use Exception;
+
 class Validation
 {
-    public static function val_action($action)
-    {
-        if (!isset($action)) {
+    public static function val_action($action): string {
+        $safeAction = htmlspecialchars($action, ENT_QUOTES);
+        if (!isset($action))
             throw new \Exception('pas d\'action');
-            //on pourrait aussi utiliser
-            //$action = $_GET['action'] ?? 'no';
-            // This is equivalent to:
-            //$action =  if (isset($_GET['action'])) $action=$_GET['action']  else $action='no';
-        }
+        else if ($safeAction != $action)
+            throw new \Exception("tentative d'injection sql détectée");
+        else return $safeAction;
+    }
+
+    public static function filter_int($value): int {
+        if ($value == null || !filter_var($value, FILTER_VALIDATE_INT) || $value < 0)
+            throw new Exception("invalid field");
+        return $value;
+    }
+
+    public static function filter_str_simple($value): string {
+        if ($value == null || !preg_match('/^[A-Za-z0-9\s\-]+$/', $value))
+            throw new Exception("invalid field");
+        return $value;
+    }
+
+    public static function filter_str_nospecialchar($value): string {
+        if ($value == null || !preg_match('/^[A-Za-z\s\-]+$/', $value))
+            throw new Exception("invalid field");
+        return $value;
     }
 
     public static function val_form(string &$nom, string &$age, &$dVueEreur)
