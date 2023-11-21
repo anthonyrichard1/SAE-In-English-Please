1254b0a81f8d4b2ca0ed0846369ec72a2e90f86b
lucie.goigoux2 <lucie.goigoux2@etu.uca.fr>
2023-11-21 11:49:12 +0100

    Création vue vocabList

--- Project/php/controller/StudentController.php
+++ Project/php/controller/StudentController.php
@@ -35,4 +35,12 @@ class StudentController extends UserController
         $vocab = $mdl->getVocabByName($name);
         echo $twig->render('manageVocabView.html', ['vocabularies' => $vocab]);
     }
+
+    public function memoryChoice(): void {
+        global $twig;
+        global $user;
+        $model = new MdlStudent();
+        $voc = $model->getAll();
+        echo $twig->render('vocabList.html', ['vocabularies' => $voc, 'userID' => $user->getId(), 'userRole' => $user->getRoles()]);
+    }
 }
