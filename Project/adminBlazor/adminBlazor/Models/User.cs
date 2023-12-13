1de23452200da80db2d06b88ce14920bc7b61b1e
pabrugiere <pabrugiere@IUTCLINFA1214>
2023-12-13 11:28:58 +0100

    ajout classe user

---
+++ Project/adminBlazor/adminBlazor/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace adminBlazor.Models
+{
+    public class User
+    {
+        private int id { get; set; }
+        private string password { get; set; }
+        private string email { get; set; }
+        private string name { get; set; }
+        private string surname { get; set; }
+        private string nickname { get; set; }
+        private string image {  get; set; }
+        private bool extraTime { get; set; }
+        private int group {  get; set; }
+        private string[] roles { get; set; }
+
+    }
+}
